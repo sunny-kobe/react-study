3c51139fc129a0d00debe8cf6c8852736e5390b3
sunyan <sunyan@joygames.cc>
2024-04-28 15:38:56 +0800

    feat: 菜谱渲染列表

---
+++ react-cli/app/components/cai.tsx
@@ -0,0 +1,43 @@
+const recipes = [
+  {
+    id: 'greek-salad',
+    name: '希腊沙拉',
+    ingredients: ['西红柿', '黄瓜', '洋葱', '油橄榄', '羊奶酪'],
+  },
+  {
+    id: 'hawaiian-pizza',
+    name: '夏威夷披萨',
+    ingredients: ['披萨饼皮', '披萨酱', '马苏里拉奶酪', '火腿', '菠萝'],
+  },
+  {
+    id: 'hummus',
+    name: '鹰嘴豆泥',
+    ingredients: ['鹰嘴豆', '橄榄油', '蒜瓣', '柠檬', '芝麻酱'],
+  },
+];
+
+
+export default function RecipeList() {
+  const cai = recipes.map(item => {
+    return (
+      <div key={item.id}>
+        <h2 >{item.name}</h2>
+        <ul>
+            {item.ingredients.map(res => {
+              return (
+                <li key={res}>
+                  {res}
+                </li>
+              )
+            })}
+        </ul>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <h1>菜谱</h1>
+      {cai}
+    </div>
+  );
+}
