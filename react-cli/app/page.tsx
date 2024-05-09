630b6d31c4fe0c305f2b91b94302e2fd35ddb7b0
sunyan <sunyan@joygames.cc>
2024-05-09 15:06:27 +0800

    feat: 更新state中的对象

--- react-cli/app/page.tsx
+++ react-cli/app/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
-import Gallery from "./components/Gallery";
-import Tab from "./components/Tab";
-import TodoList from "./components/TodoList";
-import Profile from "./components/prop";
+
+import Form from "./components/Form";
 
 
 export default function Home() {
   return (
     <main>
-      <Tab />
-      <Gallery />
-      <TodoList />
-      <Profile />
+      <Form />
     </main>
   );
 }
