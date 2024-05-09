768a546adabcb18438132cc6cb1e508f1bf4a6ce
sunyan <sunyan@joygames.cc>
2024-05-09 15:18:53 +0800

    feat: 更新一个嵌套对象

--- react-cli/app/components/Form.tsx
+++ react-cli/app/components/Form.tsx
@@ -1,51 +1,94 @@
-"use client"
+"use client";
 import { useState } from 'react';
 
 export default function Form() {
   const [person, setPerson] = useState({
-    firstName: 'Barbara',
-    lastName: 'Hepworth',
-    email: 'bhepworth@sculpture.com'
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
   });
 
-  function handleChange(e) {
+  function handleNameChange(e) {
     setPerson({
       ...person,
-      [e.target.name]: e.target.value
+      name: e.target.value
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value
+      }
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value
+      }
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value
+      }
     });
   }
 
   return (
-    <div>
+    <>
       <label>
-        First name:
+        Name:
         <input
-          name="firstName"
-          value={person.firstName}
-          onChange={handleChange}
+          value={person.name}
+          onChange={handleNameChange}
         />
       </label>
       <label>
-        Last name:
+        Title:
         <input
-          name="lastName"
-          value={person.lastName}
-          onChange={handleChange}
+          value={person.artwork.title}
+          onChange={handleTitleChange}
         />
       </label>
       <label>
-        Email:
+        City:
         <input
-          name="email"
-          value={person.email}
-          onChange={handleChange}
+          value={person.artwork.city}
+          onChange={handleCityChange}
+        />
+      </label>
+      <label>
+        Image:
+        <input
+          value={person.artwork.image}
+          onChange={handleImageChange}
         />
       </label>
       <p>
-        {person.firstName}{' '}
-        {person.lastName}{' '}
-        ({person.email})
+        <i>{person.artwork.title}</i>
+        {' by '}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
       </p>
-    </div>
+      <img 
+        src={person.artwork.image} 
+        alt={person.artwork.title}
+      />
+    </>
   );
 }
