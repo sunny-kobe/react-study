9630a5f18017851952194e68e60de7825ac72499
sunyan <sunyan@joygames.cc>
2024-05-21 18:05:08 +0800

    feat: 多选

---
+++ react-cli/app/components/select.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+const letters = [{
+  id: 0,
+  subject: 'Ready for adventure?',
+  isStarred: true,
+}, {
+  id: 1,
+  subject: 'Time to check in!',
+  isStarred: false,
+}, {
+  id: 2,
+  subject: 'Festival Begins in Just SEVEN Days!',
+  isStarred: false,
+}];
+function Letter({
+  letter,
+  onToggle,
+  isSelected,
+}) {
+  return (
+    <li className={
+      isSelected ? 'selected' : ''
+    }>
+      <label>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={() => {
+            onToggle(letter.id);
+          }}
+        />
+        {letter.subject}
+      </label>
+    </li>
+  )
+}
+
+export default function MailClient() {
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  const selectedCount = selectedIds.length;
+
+  function handleToggle(toggledId) {
+    // 它以前是被选中的吗？
+    if (selectedIds.includes(toggledId)) {
+      // Then remove this ID from the array.
+      setSelectedIds(selectedIds.filter(id =>
+        id !== toggledId
+      ));
+    } else {
+      // 否则，增加 ID 到数组中。
+      setSelectedIds([
+        ...selectedIds,
+        toggledId
+      ]);
+    }
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map(letter => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={
+              selectedIds.includes(letter.id)
+            }
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>
+            You selected {selectedCount} letters
+          </b>
+        </p>
+      </ul>
+    </>
+  );
+}
